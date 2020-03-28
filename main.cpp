7f268acefe935bcae684bd4a38d4c399e515238b
IsAyka1 <aigul-isl@yandex.ru>
2020-03-28 21:26:18 +0300

    Fractions

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by aigul on 28.03.2020.
+//
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+#include <TFraction.h>m
+
+int main() {
+    TFraction a(-9);
+    TFraction b(-6);
+    cout << (a / b);
+    return  0;
+}
